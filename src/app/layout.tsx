8f3c35cdec7fc14691e06c3abb8388e8c17c860d
stormynight9 <ferjani.nader@hotmail.fr>
2024-06-19 20:36:18 +0100

    chore: Remove favicon reference in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <head>
-                <link rel='icon' href='/favicon.ico' sizes='any' />
-            </head>
+            <head></head>
             <body className={inter.className}>
                 <Providers>
                     <EdgeStoreProvider>
